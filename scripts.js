242e02ee26508afddd3863d0bb9f56f10f23296f
Ryan Villanueva <ryanvill@gmail.com>
2016-12-30 18:03:39 -0500

    Updated logic and dom

--- scripts.js
+++ scripts.js
@@ -1,30 +1,18 @@
 'use strict';
 
 var config = {
-  eventGapSeconds: 60,
-  sensorErrorThreshold: 30,
+  timeThreshold: 60,
+  sensorThreshold: 30,
   timezoneOffset: -5,
   period: 'day',
-  colors: ['#708CF8', '#7AC943']
+  colors: ['#94aafb', '#a4e675']
 }
 
-var parsed = {
-  success: 0,
-  failed: 0
-}
-var log;
-var customers;
-var errors = [];
-var cleanedCount = 0;
-var chart;
-var eventOutput;
+//  Interactive brand colors: ['#708CF8', '#7AC943']
 
-loadEvents(function(data) {
-  eventOutput = data;
-  loadTextarea(data);
-  processInput();
-});
+var log, customers, failed, eventOutput, data, chartData, chart;
 
+// Load events from static text file
 
 function loadEvents(done) {
   var client = new XMLHttpRequest();
@@ -37,75 +25,122 @@ function loadEvents(done) {
   client.send();
 }
 
-function loadTextarea(text) {
+loadEvents(function(data) {
+  eventOutput = data;
+  updateEventInput(data);
+  processInput();
+});
+
+// Updating DOM with loaded text file
+
+function updateEventInput(text) {
   document.getElementById("event-output").value = text;
 }
 
-function loadFromTextarea() {
+// Load config file from DOM settings when running manually
+
+function loadConfig() {
   eventOutput = document.getElementById("event-output").value;
+  config.timeThreshold = Number(document.getElementById("time-threshold").value);
+  config.sensorThreshold = Number(document.getElementById("sensor-threshold").value);
+  config.timezoneOffset = Number(document.getElementById("timezone-offset").value);
 }
+
+// Process event output data
 
 function processInput() {
-  if(eventOutput){
-    log = parseEventLog(eventOutput);
-    var uncleaned = splitCustomers(log);
-    customers = clearSensorErrors(uncleaned);
-    generateCharts(customers);
-    updateDOM();
-  } else {
+  if (!eventOutput) {
     alert('Please copy event output file into the text area before running.')
+    return false;
   }
-}
-
-function parseEventLog(input) {
-  var split = input.split('\n');
-  var arr = [];
-  for (var i = 0; i < split.length; i++) {
-    var string = split[i] + 'z'; // setting these times are UTC, because it would be weird for people to be interacting at 2am Eastern
-    var date = new Date(string);
-    if (!isNaN(date)) {
-      arr.push(date);
-      parsed.success++;
-    } else {
-      parsed.failed++;
+  data = {
+    parsed: {
+      passed: 0,
+      failed: 0
+    },
+    cleaned: {
+      passed: 0,
+      failed: 0
+    },
+    totalCustomers: 0,
+    totalEngagement: 0,
+    periods: 0,
+    outputs: {
+      dateRange: '',
+      dailyCustomers: '',
+      averageEngagement: ''
     }
   }
-  return arr;
-}
+  log = parseEventLog(eventOutput);
+  var uncleaned = splitCustomers(log);
+  customers = clearSensorErrors(uncleaned);
+  generateCharts(customers);
+  calculateMetrics();
+  updateDOM();
 
-function splitCustomers(log) {
-  var customers = [];
-  var lastTime;
-  for (var i = 0; i < log.length; i++) {
-    var checked = log[i];
-    if (i === 0 || lastTime < (checked - (config.eventGapSeconds * 1000))) {
-      customers.push({
-        events: []
-      });
+  function parseEventLog(input) {
+    var split = input.split('\n');
+    var arr = [];
+    for (var i = 0; i < split.length; i++) {
+      var string = split[i] + 'z'; // setting these times are UTC, because it would be weird for people to be interacting at 2am Eastern
+      var date = new Date(string);
+      if (!isNaN(date)) {
+        arr.push(date);
+        data.parsed.passed++;
+      } else {
+        data.parsed.failed++;
+      }
     }
-    customers[customers.length - 1].events.push(checked);
-    lastTime = checked;
+    return arr;
   }
-  return customers;
-}
 
-function clearSensorErrors(customers) {
-  var cleaned = [];
-  for (var i = 0; i < customers.length; i++) {
-    var customer = customers[i];
-    if (customer.events.length <= 30) {
-      cleaned.push(customer);
-    } else {
-      errors.push(customer)
+  function splitCustomers(log) {
+    var res = [];
+    var lastTime;
+    for (var i = 0; i < log.length; i++) {
+      var checked = log[i];
+      if (i === 0 || lastTime.getTime() < (checked.getTime() - (config.timeThreshold * 1000))) {
+        res.push({
+          events: []
+        });
+      }
+      res[res.length - 1].events.push(checked);
+      lastTime = checked;
     }
+    return res;
   }
-  return cleaned;
+
+  // Clear customers who have more than a certain number of events
+  function clearSensorErrors(customers) {
+    var res = {
+      passed: [],
+      failed: []
+    };
+    for (var i = 0; i < customers.length; i++) {
+      var customer = customers[i];
+      if (customer.events.length <= config.sensorThreshold) {
+        res.passed.push(customer);
+      } else {
+        res.failed.push(customer)
+      }
+    }
+    data.cleaned = {
+      passed: res.passed.length,
+      failed: res.failed.length
+    }
+    return res.passed;
+  }
 }
 
 
+
+// Generate charts for display on webpage
 function generateCharts(customers) {
+  if(chart){
+    chart.destroy();
+  }
   var buckets = [];
-  var chartData = {
+  chartData = {
     labels: [],
     datasets: [{
       label: 'Customer Count',
@@ -123,7 +158,6 @@ function generateCharts(customers) {
   generateTimeBuckets();
   attachCustomerData();
   populateChartData();
-  console.log('building chart...')
   buildChart();
   console.log(buckets);
   console.log(chartData);
@@ -164,6 +198,7 @@ function generateCharts(customers) {
         done = true;
       }
     }
+    data.periods = buckets.length;
   }
 
   function attachCustomerData() {
@@ -172,11 +207,13 @@ function generateCharts(customers) {
       for (var j = 0; j < buckets.length; j++) {
         var bucket = buckets[j];
         if (customer.events[0].getTime() >= bucket.date.getTime() && customer.events[0].getTime() < bucket.date.getTime() + 1000 * 60 * 60 * 24) {
-          var duration = customer.events[customer.events.length - 1] - customer.events[0];
+          var duration = (customer.events[customer.events.length - 1].getTime() - customer.events[0].getTime())/60;
           bucket.data.engagementTotal += duration;
+          data.totalEngagement += duration;
           bucket.data.customerCount++;
         }
       }
+
     }
   }
 
@@ -196,7 +233,8 @@ function generateCharts(customers) {
 
   function buildChart() {
     var ctx = document.getElementById("engagement-chart");
-    var chart = new Chart(ctx, {
+
+    chart = new Chart(ctx, {
       type: 'bar',
       data: chartData,
       options: {
@@ -219,6 +257,29 @@ function generateCharts(customers) {
 }
 
 
-function updateDOM() {
+// Use combine existing data to store metrics
+function calculateMetrics(){
+  data.totalCustomers = data.cleaned.passed;
+  data.outputs.dailyCustomers = data.totalCustomers/data.periods;
+  data.outputs.averageEngagement = data.totalEngagement/data.totalCustomers/60;
+  data.outputs.dateRange = '' + chartData.labels[0] + ' - ' + chartData.labels[chartData.labels.length - 1];
+}
+
+function updateDOM(){
+  var dateRange = document.getElementById("data-date-range");
+  var customerAverage = document.getElementById("data-customer-average");
+  var engagement = document.getElementById("data-engagement");
+  var parsePassed = document.getElementById("parsed-passed");
+  var parseFailed = document.getElementById("parsed-failed");
+  var sensorPassed = document.getElementById("sensor-passed");
+  var sensorFailed = document.getElementById("sensor-failed");
+
+  dateRange.innerHTML = data.outputs.dateRange;
+  customerAverage.innerHTML = Math.floor(data.outputs.dailyCustomers);
+  engagement.innerHTML = Math.floor(data.outputs.averageEngagement) + ' mins';
+  parsePassed.innerHTML = data.parsed.passed;
+  parseFailed.innerHTML = data.parsed.failed;
+  sensorPassed.innerHTML = data.cleaned.passed;
+  sensorFailed.innerHTML = data.cleaned.failed;
 
 }
